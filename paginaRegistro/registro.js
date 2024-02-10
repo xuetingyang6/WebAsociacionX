cc617472bf5a8b8d01320ad830d4fbf85cfe2381
demo <501834377@qq.com>
2024-02-10 01:04:53 +0100

    conflictos entre el css, html, js de la página registro

--- paginaRegistro/registro.js
+++ paginaRegistro/registro.js
@@ -18,3 +18,41 @@ function showDiaSemana(show){
         diaOp.style.display = 'none';
     }
 }
+
+function showTexarea() {
+    var area=document.getElementById("motivacion");
+    var op=document.getElementById("otraMotivacion");
+    if(op.checked){
+        area.style.display='block';
+    }
+    else{
+        area.style.display='none';
+    }
+}
+
+function getDato(confirmacion){
+
+    var nombre=document.getElementById("nombre").value;
+    var apellido=document.getElementById("apellido").value;
+    var dni=document.getElementById("dni").value;
+    var email=document.getElementById("email").value;
+    var tlf=document.getElementById("tlf").value;
+
+    var datos=document.getElementById("dato");
+
+    var msg="\nNombre: "+nombre+"\nApellido: "+apellido+"\nDNI: "+dni+"\nNúmero de contacto: "+tlf+"\nCorreo electrónico: "+email;
+
+    var textNode = document.createTextNode(msg);
+    //datos.appendChild(msg);
+    datos.appendChild(textNode);
+    datos.style.display='block';
+
+    return false;
+    /*
+    confirm("Revisar los datos introducido: "+"\nNombre: "+nombre+"\nApellido: "+apellido+"\nDNI: "+dni+"\nNúmero de contacto: "+tlf+"\nCorreo electrónico: "+email);
+*/
+
+    
+
+
+}
