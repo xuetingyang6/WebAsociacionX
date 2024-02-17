400f6cce779f927b81b705c7617785948c13b3a2
demo <501834377@qq.com>
2024-02-17 19:06:01 +0100

    terminar la página actividad

--- paginaActividad/incrementa.js
+++ paginaActividad/incrementa.js
@@ -3,3 +3,17 @@ function incrementarLike(id){
     var numero=parseInt(idContador.textContent);
     idContador.textContent=numero+1;
 }
+
+
+function expandir(n){
+    var contenido=document.getElementsByClassName("contenido")[n];
+    var txt=document.getElementsByClassName("mas")[n];
+
+    if(contenido.style.display=="none"){
+        contenido.style.display="block";
+        txt.textContent="...Colapsar";
+    }else {
+        contenido.style.display="none";
+        txt.textContent="More...";
+    }
+}
